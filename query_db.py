486fcd87013278e195324c428c7a6c9746ebce4d
Sol <Sol@Solomon-Negusses-MacBook-Pro.local>
2014-10-14 21:44:35 -0500

    advanced filtering

--- query_db.py
+++ query_db.py
@@ -1,19 +1,23 @@
+from sqlalchemy import and_, not_
 import tables
 
 session = tables.Session()
 
-print "\n...filter where price == 3.49\n"
-
-for query in session.query(tables.Products).filter(tables.Products.prod_price == 3.49):
-    print query.prod_name + ' ' +  query.prod_price.to_eng_string()
-
-print "\n...filter where prod_price < 10 \n"
-
-for query in session.query(tables.Products).filter(tables.Products.prod_price < 10):
-    print query.vend_id + ' ' + query.prod_name + ' ' +  query.prod_price.to_eng_string()
-
-print "\n...filter where 5 < prod_price < 10 \n"
-
+print "\n 'AND' filtering"
 for query in session.query(tables.Products).filter(
-    tables.Products.prod_price < 10).filter(tables.Products.prod_price > 5):
-        print query.prod_name + ' ' +  query.prod_price.to_eng_string()
+	and_(tables.Products.prod_price < 4, 
+	tables.Products.vend_id == 'DLL01')):
+	print query.prod_id.strip() + ' ' + query.prod_price.to_eng_string() + \
+		' ' + query.prod_name
+
+print "\n 'IN' filtering\n"
+for query in session.query(tables.Products).filter(
+	tables.Products.vend_id.in_(['DLL01', 'BRS01'])):
+	print query.prod_name.strip() + ' ' + query.prod_price.to_eng_string()
+
+print "\n 'NOT' filtering\n"
+for query in session.query(tables.Products).filter(
+	not_(tables.Products.vend_id == 'DLL01')):
+	print query.prod_name.strip()
+
+
